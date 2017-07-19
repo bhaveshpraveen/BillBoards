4a6fd0f93fcc5af61f70331201960544c7494fa4
bhaveshpraveen <bhaveshpraveen10@gmail.com>
2017-07-20 00:39:16 +0530

    Added a shitty regex.

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from bs4 import BeautifulSoup
 import os
 import re
 
-directory_to_download_in = '/media/ichigo/Seagate Backup Plus Drive/BHAVESH/'
-os.chdir(directory_to_download_in)
-# os.mkdir('Billboards')
-os.chdir('Billboards')
+
+
+def ignore_everything_other_than_video(link):
+	count = re.match('^/watch?v=', link)
+	if count == 9:
+		return True
+	else:
+		return False
+
+def scraper(content_of_page):
+	necessary_headers = content_of_page.find_all('h3', class_="yt-lockup-title ")
+	links = [header.a.get('href') if ignore_everything_other_than_video(header.a.get('href')) for header in necessary_headers]
+	links = []
+	for header in necessary_headers:
+		watch_link = header.a.get('href')
+		if ignore_everything_other_than_video(watch_link):
+			links.append(watch_link)
+	return links
+
+
+
+# directory_to_download_in = '/media/ichigo/Seagate Backup Plus Drive/BHAVESH/'
+# os.chdir(directory_to_download_in)
+# # os.mkdir('Billboards')
+# os.chdir('Billboards')
 
 
 def formatting(name):
@@ -20,15 +41,20 @@ with open('/home/ichigo/Desktop/billboards/list') as f:
 for song_name in list_of_songs:
     song_name = formatting(song_name)
     response = requests.get('https://www.youtube.com/results?', params={'search_query': song_name})
-    soup = BeautifulSoup(response.content)
+    content = BeautifulSoup(response.content)
     ''' Change the below line alone'''
-    video_list = soup.find_all('a', class_=' yt-uix-sessionlink      spf-link ')
+    links = scraper(content)
+    
+    for link in links:
+    	entire_link = 'https://www.youtube.com{}'.format(link)
+    	print(entire_link)
+    # video_list = soup.find_all('a', class_=' yt-uix-sessionlink      spf-link ')
 
-    for i in video_list:
-        link = i.get('href')
-        print(link)
-        if link:
-            first_part = 'https://www.youtube.com'
-            entire_link = first_part + link
-            os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(entire_link))
-            break
+    # for i in video_list:
+    #     link = i.get('href')
+    #     print(link)
+    #     if link:
+    #         first_part = 'https://www.youtube.com'
+    #         entire_link = first_part + link
+    #         os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(entire_link))
+    #         break
