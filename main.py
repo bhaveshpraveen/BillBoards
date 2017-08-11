6669c3ddf9984a68cfa5fce259253c421589e45f
Bhavesh <bhaveshpraveen10@gmail.com>
2017-08-11 22:08:33 +0530

    Added Thumbnails

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import re
-import json
 
 
 def ignore_everything_other_than_video(link):
@@ -13,14 +12,6 @@ def ignore_everything_other_than_video(link):
         return False
 
 
-# def extract_view_count_from_url(url):
-#     data_object = os.popen('youtube-dl -J "{}"'.format(url))
-#     data = data_object.readline()
-#     data_dictionary = json.loads(data)
-#     ''' If view_count not present, default to None '''
-#     return data_dictionary.get('view_count', None)
-
-
 def formatting_views(views_string):
     return int(views_string.split()[0].replace(',', ''))
 
@@ -28,10 +19,9 @@ def formatting_views(views_string):
 def scraper(content_of_page):
     ''' Change the line below for discrepancies in scraping '''
     divisions = content_of_page.find_all('div', class_="yt-lockup-content")
-    # print(divisions)
     max_views = 0
     d = {}
-    # links = [header.a.get('href') if ignore_everything_other_than_video(header.a.get('href')) for header in necessary_headers]
+
     for division in divisions:
         watch_link = division.h3.a.get('href')
         if not ignore_everything_other_than_video(watch_link):
@@ -43,8 +33,6 @@ def scraper(content_of_page):
         except:
             continue
 
-        # print(watch_link)
-
         if max_views > views:
             continue
 
@@ -54,12 +42,6 @@ def scraper(content_of_page):
             d = {'title': title, 'link': watch_link, 'views': views}
 
     return d
-
-
-# directory_to_download_in = '/media/ichigo/Seagate Backup Plus Drive/BHAVESH/'
-# os.chdir(directory_to_download_in)
-# # os.mkdir('Billboards')
-# os.chdir('Billboards')
 
 
 def formatting(name):
@@ -84,37 +66,12 @@ songs_not_downloaded = []
 for song_name in list_of_songs:
     song_name = formatting(song_name)
     response = requests.get('https://www.youtube.com/results?', params={'search_query': song_name})
-    content = BeautifulSoup(response.content)
+    content = BeautifulSoup(response.content, 'lxml')
     proper_video = scraper(content)
     if not proper_video:
         songs_not_downloaded.append(song_name)
         continue
-    print(proper_video)
-    # os.system("youtube-dl --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s' {}".format(proper_video['watch_link']))
+    entire_link = "https://www.youtube.com{}".format(proper_video['link'])
+    command = "youtube-dl -q --extract-audio --audio-format mp3 --xattrs --embed-thumbnail --audio-quality 0  -o '%(title)s.%(ext)s' '{}'".format(entire_link)
+    os.system(command)
 
-    # print(links)
-    # max_views = 0
-    # dictionary_with_views = {}
-    # for link in links:
-    #     entire_link = 'https://www.youtube.com{}'.format(link)
-    #     views = extract_view_count_from_url(entire_link)
-    #     if not views:
-    #         continue
-    #     dictionary_with_views[views] = entire_link
-    #     if max_views < views:
-    #         max_views = views
-    # print(entire_link, dictionary_with_views)
-    # link_to_download = dictionary_with_views[max_views]
-    # print(link_to_download)
-    # os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(link_to_download))
-
-    # video_list = soup.find_all('a', class_=' yt-uix-sessionlink      spf-link ')
-
-    # for i in video_list:
-    #     link = i.get('href')
-    #     print(link)
-    #     if link:
-    #         first_part = 'https://www.youtube.com'
-    #         entire_link = first_part + link
-    #         os.system("youtube-dl --extract-audio --audio-format mp3 {}".format(entire_link))
-    #         break
